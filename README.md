11edf653bf8d8ca3829cc26a6a8add63c9aeb2c6
alex <jiahangsocket@163.com>
2020-07-25 20:04:53 +0800

    解释

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# appstation
+### 此程序已成功安装在Android IOS 真机进行测试
 
-A new Flutter application.
+###概述
+-android文件夹：android平台相关代码
+-ios：ios平台相关代码
+-lib：flutter相关代码，我们主要编写的代码就在这个文件夹
+-test：用于存放测试代码
+-pubspec.yaml：配置文件，一般存放一些第三方的依赖。
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+官方开发指南：(https://flutter.dev/docs/cookbook)
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
+//此行代码作用是导入了Material UI组件库。Material是一种标准的移动端和web端的视觉设计语言，
+// Flutter默认提供了一套丰富的Material风格的UI组件。
 import 'package:flutter/material.dart';
 
+
+//应用入口
 void main() {
   runApp(MyApp());
 }
 
+//这里相当于Android类的Application类 StatelessWidget表示无状态组件
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    /**
+     * MaterialApp表名App的风格是Material Design风格的
+     * 这里我们可以设置App的主题相关属性，比如颜色，按钮风格等等，类似于Android中的style文件
+     */
     return MaterialApp(
       title: 'Flutter Demo',
+
+      /**
+       * 主题相关配置
+       * 这里我们可以配合App整体的主题样式，比如整体颜色，控件默认的样式等
+       */
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,18 +35,32 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        /**
+         * 这个地方就类似与Android中的style文件配置，主要就是主题的配置
+         */
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      /**
+       * home指定启动后显示的界面
+       * 类似AndroidManifest中配置启动界面
+       */
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+
+/// StatefulWidget是一个有状态的控件，如果你的页面需要更新UI，那么该页面就要继承StatefulWidget
+/// 如果只是一个纯展示的页面，只要继承自StatefulWidget即可
+/// MyHomePage在上面已经被指定为启动页面
+/// 该页面有一个计数的逻辑，会更新UI，所以需要继承StatefulWidget
 class MyHomePage extends StatefulWidget {
+  /// Dart中的构造方法 {}里面的参数表示可选参数，跟kotlin概念差不多
+  /// this.title默认会把值赋给下面的参数title
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -43,12 +72,27 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
+  ///声明一个变量 用final修饰表示只能赋一次值
+  ///在上面的构造方法中已经有赋值操作了，所以后面我们就不能再更改title的值了
   final String title;
 
+  ///所有继承自StatefulWidget的空间都要重写createState()这个方法
+  ///可以理解为指定该页面的状态是由谁来控制的
+  ///在Dart中下划线开头声明的变量和方法，其默认访问权限就是私有的
+  ///类似java中的private修饰，只能在类的内部访问
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
+
+///MyHomePage是应用的首页，它继承自StatefulWidget类，表示它是一个有状态的widget（Stateful widget）。
+///现在，我们可以简单认为Stateful widget 和Stateless widget有两点不同：
+//Stateful widget可以拥有状态，这些状态在widget生命周期中是可以变的，而Stateless widget是不可变的。
+//
+//Stateful widget至少由两个类组成：
+//      一个StatefulWidget类。
+//      一个State类；StatefulWidget类本身是不变的，但是 State类中持有的状态在widget生命周期中可能会发生变化。
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
